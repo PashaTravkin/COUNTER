ed98de1bd7f94b18ce113d6a2ec91c7b3c168ea3
PashaTravkin <refueller2022@bk.ru>
2022-07-03 12:57:18 +0300

    IncreaseReset component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {IncreaseResetPropsType} from "./IncreaseResetPropsType/IncreaseResetPropsType";
+
 
 function App() {
-  return (
-    <div className="App">
-     <div>Empty App</div>
-    </div>
-  );
+
+    type valueType = number
+    let [value, setValue] = useState<valueType>(0)
+
+    return (
+        <div className="App">
+            <div className={"counterWrapper"}>
+                <div className={'windowChangingNumber'}>
+                    <div className={'changingNumber'}><span className={value == 5 ? 'fiveRed' : ''}>{value}</span></div>
+                </div>
+                <div className={'buttons'}>
+                    <IncreaseResetPropsType value={value} setValue={setValue} title={'inc'}/>
+                    <IncreaseResetPropsType value={value} setValue={setValue} title={'reset'}/>
+                    {/*<Reset value={value} setValue={setValue} title={'reset'}/>*/}
+                </div>
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
