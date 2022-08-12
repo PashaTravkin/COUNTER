af293af67b3194eb62fce23004c81e2749739c1c
PashaTravkin <refueller2022@bk.ru>
2022-08-12 13:18:39 +0300

    supper Counter1 with localStorage for Setters valueMax

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,19 @@ import Setter from "./Setter/Setter";
 
 function App() {
 
-    let [value, setValue] = useState<number>(0)
-    let [valueMaxCounter, setValueMaxCounter] = useState(4)
-    let [startValue, setStartValue] = useState(0)
-    let [valueMax, setValueMax] = useState(0)
+    let [value, setValue] = useState<number>(0)// то, что мы видем в счетчике
+    let [valueStartCounter, setValueStartCounter] = useState(0)// это для хранения стартового значения счетчика
+    let [valueMaxCounter, setValueMaxCounter] = useState(4)// это для хранения максимального значения для установки счетчика
+    let [valueMax, setValueMax] = useState(0)// это для хранения мах значения, устанавливаемого в setter-e
 
     return (
         <div className="App">
-            <Setter valueMax={valueMax} startValue={startValue} setValueMax={setValueMax}
+            <Setter valueMax={valueMax} valueStartCounter={valueStartCounter} setValueMax={setValueMax}
                     setValueMaxCounter={setValueMaxCounter}
-                    setStartValue={setStartValue}
+                    setValueStartCounter={setValueStartCounter}
                     setValue={setValue}
             />
-            <Counter value={value} setValue={setValue} valueMaxCounter={valueMaxCounter} startValue={startValue}/>
+            <Counter value={value} setValue={setValue} valueMaxCounter={valueMaxCounter} startValue={valueStartCounter}/>
         </div>
     )
 }
--- src/Setter/Setter.tsx
+++ src/Setter/Setter.tsx
@@ -1,22 +1,35 @@
 import s from './Setter.module.css';
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, useEffect, useState} from "react";
 import {Button} from "../Button/Button";
 
 type SetterPropsType = {
     valueMax: number
-    startValue: number
+    valueStartCounter: number
     setValueMax: (valueMax: number) => void
     setValueMaxCounter: (setValueMaxCounter: number) => void
-    setStartValue: (startValue: number) => void
+    setValueStartCounter: (startValue: number) => void
     setValue: (value: number) => void
 }
 
 function Setter(props: SetterPropsType) {
 
     let [valueSetterMax, setValueSetterMax] = useState(props.valueMax)
-    let [valueStart, setValueStart] = useState(props.startValue)
+    let [valueStart, setValueStart] = useState(props.valueStartCounter)
 
-    const onChangeHandlerMax = (event: ChangeEvent<HTMLInputElement>) => {
+    useEffect(() => {
+        let holdValue = localStorage.getItem('setElementMax')
+        if (holdValue) {
+            let newHoldValue = JSON.parse(holdValue)
+            props.setValueMax(newHoldValue)
+            props.setValueMaxCounter(newHoldValue)
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('setElementMax', JSON.stringify(props.valueMax))
+    }, [props.valueMax])
+
+       const onChangeHandlerMax = (event: ChangeEvent<HTMLInputElement>) => {
         const number = event.currentTarget.value
         number && props.setValueMax(Number(number))
         setValueSetterMax(Number(number))
@@ -24,13 +37,14 @@ function Setter(props: SetterPropsType) {
 
     const onChangeHandlerStart = (event: ChangeEvent<HTMLInputElement>) => {
         const number = event.currentTarget.value
-        number && props.setStartValue(Number(number))
+        number && props.setValueStartCounter(Number(number))
         setValueStart(Number(number))
     }
 
     let onclickHandler = () => {
-        props.setValueMaxCounter(valueSetterMax)
+        props.setValueMaxCounter(props.valueMax)
         props.setValue(valueStart)
+        props.setValueStartCounter(props.valueStartCounter)
     }
 
     return (
@@ -41,7 +55,7 @@ function Setter(props: SetterPropsType) {
                         <span>max value: </span> <input type={'number'} onChange={onChangeHandlerMax}
                                                         value={props.valueMax}/>
                         <span>start value: </span> <input type={'number'} onChange={onChangeHandlerStart}
-                                                          value={props.startValue}/>
+                                                          value={props.valueStartCounter}/>
                     </div>
                 </div>
                 <div className={s.buttons}>
